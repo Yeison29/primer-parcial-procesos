ff648108c0337ad0042fe48abbc6af9111b871f5
Josue <jcampos@ufpso.edu.co>
2022-10-06 20:04:07 -0500

    Correción en la conexion

--- src/main/java/com/primerparcialpn/articulos/models/Articulo.java
+++ src/main/java/com/primerparcialpn/articulos/models/Articulo.java
@@ -13,21 +13,22 @@ import java.util.Date;
 public class Articulo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long codigo;
+    private Long id;
+    @Column(length = 50, unique = true)
+    private String codigo;
     @Column(length = 100, nullable = false)
     private String nombre;
     @Column(length = 500)
     private String descricion;
     @Column(nullable = false)
     private Date fechaRegistro;
-
-
     @Column(length = 50, nullable = false)
     private int stock;
     @Column(length = 100, nullable = false)
     private double precioVenta;
     @Column(length = 100, nullable = false)
     private int precioCompra;
+    @ManyToOne
     private  Categoria categoria;
 
     /*código, nombre, descripción, fecha de registro,
