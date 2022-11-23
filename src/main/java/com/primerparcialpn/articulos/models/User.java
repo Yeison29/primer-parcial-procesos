86281b2b9c7152c919471a1ad37a20e9347a5d18
Josue0625 <jcampos@ufpso.edu.co>
2022-11-23 16:27:46 -0500

    Elimacion de importaciones no utilizadas

--- src/main/java/com/primerparcialpn/articulos/models/User.java
+++ src/main/java/com/primerparcialpn/articulos/models/User.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
