12be54eb94d7292a9c9804aa558123a50c69ef67
YEISON ASCANIO <ydascanioa@ufpso.edu.co>
2022-11-23 14:54:19 -0500

    Se agrego la clase authController

---
+++ src/main/java/com/primerparcialpn/articulos/controllers/AuthController.java
@@ -0,0 +1,19 @@
+package com.primerparcialpn.articulos.controllers;
+
+import com.primerparcialpn.articulos.models.User;
+import com.primerparcialpn.articulos.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+    @Autowired
+    private UserService userService;
+    @PostMapping(value = "auth/login")
+    public ResponseEntity login (@RequestBody User user){
+        return userService.login(user.getEmail(), user.getPassword());
+    }
+}
